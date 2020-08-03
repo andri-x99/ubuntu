df3619495bd91d39fa6fbb669288343c267db0fe
andri-x99 <sapoetraespada99@gmail.com>
2020-08-03 06:51:35 +0000

    skripsi

---
+++ ping.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Raspberry"
+ping 10.33.109.242 -c 1
+echo ""
+echo "Dionaea Murni OpenStack"
+ping 10.33.109.144 -c 1
+echo ""
+echo "Dionaea Edit OpenStack"
+ping 10.33.109.140 -c 1
+echo ""
+echo "Cowrie OpenStack"
+ping 10.33.109.137 -c 1
+echo ""
+
+
